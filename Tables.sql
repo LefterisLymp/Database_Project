1f4d159f2d71bc64edeff7f66f47f75c54f199a4
el2kaps <63153771+el2kaps@users.noreply.github.com>
2020-05-04 19:35:46 +0300

    Update and rename DatabasesProject.sql to Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -50,4 +50,14 @@ create table contains_ (
        foreign key(barcode) references product(barcode)
        );
         
+create table transaction (
+       Payment_method varchar(10),
+       Total_amount numeric(6,2),
+       Total_pieces numeric(3,0),
+       card_num char(7),
+       date_time date,
+       primary key (card_num),
+       foreign key (card_num) references contain(card_num),
+       foreign key (date_time) references contain(date_time)
+       );
        
