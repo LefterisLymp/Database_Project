e55cff97b037bb1fde6907afeb0b63d1ebc01757
Lefteris Lymperopoulos <64773191+LefterisLymp@users.noreply.github.com>
2020-05-08 19:37:06 +0300

    Add files via upload

---
+++ inserting_data.sql
@@ -0,0 +1,44 @@
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (8726109, 'Savvas Palamaropoulos', '1987-04-23', 76, 2103416340, 'Athens', 3);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000001, 'Pericles Rossoulis', '1994-02-23', 23, 2103416540, 'Athens', 4);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000002, 'Vaggelis Toccelis', '1994-12-21', 53, 2103496540, 'Athens', 4);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000003, 'Tryfon Kostotis', '1971-10-15', 153, 2103493540, 'Athens', 1);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000004, 'Thomas Mavrotis', '1999-10-15', 42, 2103412540, 'Athens', 2);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000005, 'Georgia Gabrea', '1992-08-01', 64, 2103499870, 'Volos', 1);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000006, 'Aphrodite Vallatou', '1995-10-15', 83, 2113493540, 'Kalamata', 1);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000007, 'Hermione Galanide', '2000-12-05', 17, 2129493540, 'Athens', 1);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000008, 'Aikaterini Andreadakou', '1990-10-31', 69, 2103403540, 'Athens', 1);
+INSERT INTO customer(card_num, customer_name, birth_date, points, phone, city, family_members) values (0000009, 'Kyriake Kalloglou', '1988-10-25', 1742, 2103454540, 'Athens', 1);
+
+INSERT INTO stores VALUES ('ATH01', 3000, '09:00:00', '20:00:00', 'Gramou', 19, 15267, 'Athens');
+INSERT INTO stores VALUES ('ATH02', 1500, '09:30:00', '20:00:00', 'Smolika', 21, 15287, 'Athens');
+INSERT INTO stores VALUES ('ATH03', 1000, '09:30:00', '20:00:00', 'Parnonos', 256, 14321, 'Athens');
+INSERT INTO stores VALUES ('ATH04', 700, '10:00:00', '20:30:00', 'Olympou', 3, 15067, 'Athens');
+INSERT INTO stores VALUES ('ATH05', 1500, '09:00:00', '20:00:00', 'Taygetou', 4, 15864, 'Athens');
+INSERT INTO stores VALUES ('PAT01', 3000, '08:30:00', '21:00:00', 'Parnithas', 5, 19467, 'Patra');
+INSERT INTO stores VALUES ('PAT02', 1200, '09:30:00', '20:00:00', 'Itis', 21, 15267, 'Patra');
+INSERT INTO stores VALUES ('PAT03', 1500, '09:30:00', '20:00:00', 'Psiloriti', 42, 15255, 'Patra');
+INSERT INTO stores VALUES ('THE01', 1300, '09:00:00', '20:00:00', 'Rodopis', 17, 19267, 'Thessaloniki');
+INSERT INTO stores VALUES ('THE02', 1500, '09:00:00', '20:00:00', 'Pindou', 69, 21267, 'Thessaloniki');
+
+INSERT INTO store_phone VALUES ('ATH01', 6976543210);
+INSERT INTO store_phone VALUES ('ATH01', 6976543211);
+INSERT INTO store_phone VALUES ('ATH01', 6976543212);
+INSERT INTO store_phone VALUES ('ATH01', 6976543213);
+INSERT INTO store_phone VALUES ('ATH02', 6976543214);
+INSERT INTO store_phone VALUES ('ATH02', 6976543215);
+INSERT INTO store_phone VALUES ('ATH03', 6976543216);
+INSERT INTO store_phone VALUES ('ATH03', 6976543217);
+INSERT INTO store_phone VALUES ('ATH04', 6976543218);
+INSERT INTO store_phone VALUES ('ATH05', 6976543219);
+INSERT INTO store_phone VALUES ('PAT01', 6976543220);
+INSERT INTO store_phone VALUES ('PAT01', 6976543221);
+INSERT INTO store_phone VALUES ('PAT01', 6976543222);
+INSERT INTO store_phone VALUES ('PAT01', 6976543223);
+INSERT INTO store_phone VALUES ('PAT02', 6976543224);
+INSERT INTO store_phone VALUES ('PAT02', 6976543225);
+INSERT INTO store_phone VALUES ('PAT03', 6976543226);
+INSERT INTO store_phone VALUES ('THE01', 6976543227);
+INSERT INTO store_phone VALUES ('THE02', 6976543228);
+INSERT INTO store_phone VALUES ('THE02', 6976543229);
+
+
