60a0f23d4557ac6ed33a2808bac2ea39aa6f5105
Lefteris Lymperopoulos <64773191+LefterisLymp@users.noreply.github.com>
2020-07-05 13:44:05 +0300

    Add files via upload

--- creating_database/Exer6/queries_for_6a.sql
+++ creating_database/Exer6/queries_for_6a.sql
@@ -1,33 +1,51 @@
-select T.store_id, T.date_time
+/*select T.store_id, T.date_time
 from stores as S, transaction_ as T
 where S.store_id = T.store_id;
+*/
 
 /*All datetimes with transactions in ATH01*/
-select date_time
-from transaction_
+
+
+select contain.barcode, contain.pieces, contain.date_time
+from transaction_ natural join contain
 where store_id = 'ATH01';
 
+select contain.barcode, contain.pieces, contain.date_time
+from transaction_ natural join contain
+where store_id = 'ATH01' and CAST(date_time as DATE) between '2015-1-1' and '2020-1-1';
+
 /*Same store, same date 6a1*/ 
-select date_time 
-from transaction_
-where store_id = 'ATH01' and (CAST(date_time AS DATE) = CAST('2015-7-27' AS DATE));
+
+select contain.barcode, contain.pieces, contain.date_time
+from transaction_ natural join contain
+where store_id = 'ATH01' and (CAST(date_time AS DATE)) = '2015-7-27';
+
+
 
 /*Same store, total_pieces*/
-select Total_pieces.DateT
-from Total_pieces join transaction_ on (Total_pieces.CardNo = transaction_.card_num and Total_pieces.DateT = transaction_.date_time)
-where transaction_.store_id = 'ATH01' and (Total_pieces.Totpcs < 50);
+
+select contain.barcode, contain.pieces, contain.date_time
+from transaction_ natural join contain natural join total_pieces
+where transaction_.store_id = 'ATH01' and (total_pieces.totpcs > 5);
+
+
 
 /*Same store, total cost*/
-SELECT date_time
-FROM transaction_
-WHERE transaction_.store_id = 'ATH01' AND (transaction_.Total_amount = 35.03);
+
+select contain.barcode, contain.pieces, contain.date_time
+from transaction_ natural join contain
+WHERE transaction_.store_id = 'ATH01' AND (transaction_.Total_amount between 30 and 50);
+
  
  /*Same store, same payment method*/
-select date_time
-from transaction_
-where transaction_.store_id = 'ATH01' and (transaction_.payment_method = 'card');
+
+select contain.barcode, contain.pieces, contain.date_time
+from transaction_ natural join contain
+where transaction_.store_id = 'ATH01' and (transaction_.payment_method = 'cash') and CAST(date_time as DATE) between '2015-1-1' and '2020-1-1';
+
 
 /*Same store, same category*/
-select distinct transaction_.date_time
+
+select contain.barcode, contain.date_time, contain.pieces
 from (transaction_ natural join contain) join product on (contain.barcode = product.barcode)
 where (transaction_.store_id = 'ATH01' and product.category_id = 4);
