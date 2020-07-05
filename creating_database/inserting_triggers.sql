d6bc3a9495626b7ddbb65c07d395206bba1ade44
Lefteris Lymperopoulos <64773191+LefterisLymp@users.noreply.github.com>
2020-07-05 18:28:56 +0300

    Add files via upload

---
+++ creating_database/inserting_triggers.sql
@@ -0,0 +1,33 @@
+DROP TRIGGER update_points;
+DROP TRIGGER delete_product;
+DROP TRIGGER inserting_into_older_prices;
+
+delimiter |
+CREATE TRIGGER update_points AFTER INSERT ON transaction_
+       FOR EACH ROW
+       BEGIN
+         UPDATE customer SET points = points + FLOOR((new.Total_amount)/10) WHERE card_num = new.card_num;
+END;
+
+|
+
+CREATE TRIGGER delete_product BEFORE DELETE ON product
+FOR EACH ROW
+BEGIN
+  /*We delete all the tuples in "offers" related to this product*/
+  DELETE FROM offers WHERE barcode = old.barcode;
+  /*We want to mantain information for old products*/
+END;
+
+|
+
+CREATE TRIGGER inserting_into_older_prices AFTER UPDATE ON product
+	FOR EACH ROW
+	BEGIN
+		IF (old.price <> new.price) THEN
+			UPDATE older_prices
+				SET end_date = CURRENT_DATE
+			WHERE (product.barcode = older_prices.barcode and end_date = NULL);
+			INSERT INTO older_prices (barcode, start_date, price) values (new.barcode, CURRENT_DATE, new.price);
+		END IF;
+END;
